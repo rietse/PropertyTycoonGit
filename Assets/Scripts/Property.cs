d488f9e973b25c1d90d4d6d4ad47f9b677f4ad5c
Sean Wadsworth <sw695@sussex.ac.uk>
2022-04-20 16:03:44 +0100

    starting to redo what was lost in the move to git :,)

---
+++ Assets/Scripts/Property.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Property : Space
+{
+    public enum Colour { BROWN, BLUE, PURPLE, ORANGE, RED, YELLOW, GREEN, DBLUE}
+    public Colour colour;
+
+    public int price;
+    public int rent0;
+    public int rent1;
+    public int rent2;
+    public int rent3;
+    public int rent4;
+    public int rent5;
+
+    public int developmentLevel = 0;
+}
