0901768a75da15befe3ea9e00b5934ebf1d31eed
rietse <rietse68@gmail.com>
2022-04-22 00:47:00 +0100

    display the cash money
    thats a lot of text mesh pro files

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,20 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public Board board;
     public MovementController mc;
     private int currentPos;
+    private int currentMoney;
+    public TextMeshProUGUI moneyText;
+
     void Start()
     {
+        currentMoney = 1500;
+        SetMoneyText();
     }
 
 
     void Update()
     {
         
+    }
+    void SetMoneyText()
+    {
+        moneyText.text = "Current Money:" + currentMoney.ToString();
     }
 
     public void Move()
